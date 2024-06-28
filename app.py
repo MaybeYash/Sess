6b6a14b7631a7627205a5a1b0fb5315c6664b4c6
Yᴀsʜ <unknownsoumo@gmail.com>
2024-06-28 23:12:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+
+app = Client(
+    name="han",
+    api_id=29400566,
+    api_hash="8fd30dc496aea7c14cf675f59b74ec6f",
+    session_string="BQHAnfYAUnSXLtwDM0PSLWrLLEVf4vD0qxvJcUpQOTynTXwW3Y5SazJ7ZJR2Ew4yFwF9g8X_mDzL1acd0153rkgbQG_dL2g2YkXQ-U91FtLY5EaQ4sJkDaMhzgohf4syIgYBCmmkQBFzkBieFQk5ItfMhz3Ct3aRyt5Ayx2hL2mkcePUivKi6mHUq9WjoPOeh69ANuQZETCo3oNybeOA3DRQ0arGnH2WDkvbK1hP6-rMY_uPM8bq1lB8iVtKShTg2CYnvmgeeLj2HSjlblTn15jnj4r1x0m59z_z-lKho4H169YEceNEZjG9DDwbcaWhTYj4NLedQfK5YDMqr4kChdu4NtC8-QAAAAGDvCC_AA"
+)
+
+app.run()
